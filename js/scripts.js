fb8b188da361f76beeebd081de15242d8605a337
Brett New and Caleb Stokka <caleb@>
2016-05-31 14:13:54 -0700

    added toggle to highlight paragraphs

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#p1").click(function() {
+    $("#p1").toggleClass("highlight")
+  });
+  $("#p2").click(function() {
+    $("#p2").toggleClass("highlight")
+  });
+});
